985910f80e229b2be3c1a6a13c864d2935c35bef
lznicolas <nicolaslamas66@gmail.com>
2025-05-08 00:47:34 -0300

    Agregar pipeline con jacoco

--- src/main/java/com/tallerherramientas/tallerprueba/Services/Implementaciones/Trabajo/TrabajoDAOImp.java
+++ src/main/java/com/tallerherramientas/tallerprueba/Services/Implementaciones/Trabajo/TrabajoDAOImp.java
@@ -1,29 +1,36 @@
 package com.tallerherramientas.tallerprueba.Services.Implementaciones.Trabajo;
 
 import com.tallerherramientas.tallerprueba.Modelo.Entities.Trabajo;
+import com.tallerherramientas.tallerprueba.Repositories.TrabajoRepository;
 import com.tallerherramientas.tallerprueba.Services.Contratos.TrabajoDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class TrabajoDAOImp implements TrabajoDAO {
+    @Autowired
+    private TrabajoRepository trabajoRepository;
+
     @Override
     public Trabajo guardar(Trabajo entidad) {
-        return null;
+        return trabajoRepository.save(entidad);
     }
 
     @Override
     public List<Trabajo> listar() {
-        return null;
+        return trabajoRepository.findAll();
     }
 
     @Override
     public Optional<Trabajo> obtenerPorId(Long aLong) {
-        return Optional.empty();
+        Optional<Trabajo> trabajo = trabajoRepository.findById(aLong);
+        return Optional.ofNullable(trabajo.orElse(null));
     }
 
     @Override
     public void eliminar(Long aLong) {
-
+        trabajoRepository.deleteById(aLong);
     }
 }
